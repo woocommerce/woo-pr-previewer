954dd23fe4a41948056c751d7176d0bf20014dd1
Darren Ethier <darren@roughsmootheng.in>
2023-06-26 17:19:23 -0400

    Update middleware.ts (remove debug)

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
-  console.log(request.nextUrl.pathname);
   if (request.nextUrl.pathname.startsWith("/plugin-proxy")) {
     response.headers.append("Access-Control-Allow-Origin", "*");
   }
